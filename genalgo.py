1177e4b8a9b978318ad11145100e684a16688eba
TheDerkus <7144453+TheDerkus@users.noreply.github.com>
2023-08-18 15:33:42 -0400

    Add files via upload
    refactoring

--- genalgo.py
+++ genalgo.py
@@ -16,27 +16,23 @@ class Generation:
     def collapse(self, p):
         return uniform(0, 1) < p
 
-    def mutates(self):
-        return self.collapse(MUTATION_RATE)
-
-    def vary(self, p):
-        if not self.mutates():
+    def mutate(self, p):
+        if not self.collapse(MUTATION_RATE):
             return p
         return max(0, min(1, p + self.delta()))
 
     def normalize(self, t):
         return [p/sum(t) for p in t]
     
-    def mutate(self, i):
-        for idx in range(2):
-            i[idx] = self.vary(i[idx])
-        return self.normalize(i)
+    def reproduce(self, i):
+        v = [self.mutate(x) for x in i]
+        return self.normalize(v)
 
     def survives(self, r):
         return self.collapse(r[0]/MAX_SCORE)
 
     def offspring(self, s):
-        return [self.mutate(s) for _ in range(NUM_CHILDREN)]
+        return [self.reproduce(s) for _ in range(NUM_CHILDREN)]
 
     def next(self):
         survivors = filter(self.survives, self.results)
